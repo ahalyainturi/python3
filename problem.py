0ae28a5d69e35bb1b56832c942cabbf3f1a82fa0
ahalyainturi <130925016+ahalyainturi@users.noreply.github.com>
2023-04-16 23:10:24 +0530

    Create problem.py

---
+++ problem.py
@@ -0,0 +1,14 @@
+
+##print(1+1)
+##print(2*10)
+##print((10*10)+100)
+
+print('new line begins')
+first_number = 1+1+5
+print('first_number' , first_number)
+second_number = 105+10
+print('second_number', second_number)
+
+total = first_number + second_number
+
+print('total number', total)
